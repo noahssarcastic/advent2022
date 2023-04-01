35759ee407a150ae0e4458b3d7a82a5def779fbe
Noah Iniguez <niniguezg@gmail.com>
2023-04-01 15:16:34 -0500

    Parse input 15.1

--- 16/main.go
+++ 16/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"runtime/pprof"
+)
 
+var input = flag.String("f", "input.txt", "input file")
+var prof = flag.String("profile", "", "write cpu profile to file")
+
+func main() {
+	flag.Parse()
+	if *prof != "" {
+		f, err := os.Create(*prof)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	re, err := regexp.Compile(
+		`Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? ((?:[A-Z][A-Z], )*[A-Z][A-Z])`,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Open(*input)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		match := re.FindAllStringSubmatch(scanner.Text(), 1)
+		fmt.Println(match[0][1:])
+	}
+	if err = scanner.Err(); err != nil {
+		panic(err)
+	}
 }
